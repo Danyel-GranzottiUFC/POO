f174aefe14146e98b53b2b6ad2d47e58a7e80ea7
Guilherme Augusto <guiidiasdc@gmail.com>
2022-01-27 23:22:02 -0300

    Update DRIVER.h

--- DRIVER.h
+++ DRIVER.h
@@ -1,34 +1,15 @@
 #ifndef DRIVER_H
 #define DRIVER_H
+/* Headers */
+#include "MIDIA.h"
+#include "DVD.h"
+#include "CD.h"
 
+/* Bibliotecas C++ */
 #include <iostream>
-
-using std::cout;
-using std::cin;
-using std::endl;
-
 #include <deque>
 #include <iterator>
-#include "./MIDIA.h"
 
+/* Inicio das funções */
 
-
-class DRIVER{
-    private:
-    std::deque<MIDIA*>;
-}
-
-// Dado o nome do artista, exibir o nome de todos os CDs de sua autoria, ordenados pela data de lançamento;
-void printCDs(string name)
-{
-    for (w = minhaFila.begin(); w != minhaFila.end(); w++)
-        cout << "A fila está assim...: " << *w << endl;
-}
-//Vou pensar um pouco aqui como fazemos
-
-/* Dado um ano, exibir todas as mídias lançadas naquele ano, idependente do artista, ordenados alfabeticamentes (Não haverá distinção entre CD e DVD)*/
-void printAllMedia(int year){
-    // vou da uma pesquisada aqui tbm
-    // (y)
-};
 #endif

d1ed599c8a5b71b129a017162e5b484464976ffa
Guilherme Augusto <guiidiasdc@gmail.com>
2022-01-27 23:22:15 -0300

    Update CD.h

--- CD.h
+++ CD.h
@@ -1,8 +1,17 @@
 #ifndef CD_H
 #define CD_H
-using namespace std;
 
+/* Headers */
+#include "MIDIA.h"
+#include "DVD.h"
+
+/* Bibliotecas */
+#include <iostream>
+#include <string>
 #include <deque>
+#include <iterator>
+
+using namespace std;
 
 class CD{
 private:
@@ -25,23 +34,23 @@ public:
 
 int CD::getDuracao(){
    return (this->duracao);
-}
+};
 void CD::setDuracao(int n){
    this->duracao=n;
-}
+};
 
 float CD::getVolume(){
    return (this->volume);
-}
+};
 void CD::setVolume(float n){
    this->volume=volume;
-}
+};
 
 bool CD::getColetanea(){
    return (this->coletanea);
-}
+};
 void CD::setColetanea(bool n){
    this->coletanea=n;
-}
+};
 
 #endif
